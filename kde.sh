e7345ec196a0a62956cfbdf342e54b4c2a66da78
atreb <bhupendra.atre@dowjones.com>
2016-01-03 14:44:27 -0500

    removed k3b

--- kde.sh
+++ kde.sh
@@ -1,6 +1,5 @@
 #KDE install
-pacman -S --noconfirm sddm plasma
-(echo 2; echo Y) | pacman -S kcalc konsole kwrite dolphin ark p7zip zip unzip unrar gwenview qt5-imageformats kimageformats k3b dvd+rw-tools vcdimager transcode emovix jdk8-openjdk icedtea-web libreoffice-fresh hyphen-en libmythes mythes-en kdegraphics-okular simplescreenrecorder amarok ktorrent partitionmanager ntfs-3g dosfstools ncdu cups hplip print-manager sane xsane firefox flashplugin gtk-theme-orion cronie
+pacman -S --noconfirm sddm plasma kcalc konsole kwrite dolphin ark p7zip zip unzip unrar gwenview qt5-imageformats kimageformats jdk8-openjdk icedtea-web libreoffice-fresh hyphen-en libmythes mythes-en kdegraphics-okular simplescreenrecorder amarok ktorrent partitionmanager ntfs-3g dosfstools ncdu cups hplip print-manager sane xsane firefox flashplugin cronie
 systemctl enable sddm
 systemctl enable NetworkManager
 systemctl enable org.cups.cupsd
